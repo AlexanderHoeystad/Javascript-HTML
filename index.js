3af0dd8110933840b0be3f0974ec04abd5b20aa0
AlexanderHoeystad <Alek369@hotmail.com>
2024-04-30 13:03:36 +0200

    HTML og JS lavet på GetAll

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 const baseurl = "https://drapialexogphilip.azurewebsites.net/api/MusicRecords"
+Vue.createApp({
+    data() {
+        return {
+            MusicRecords: [],
+            // UpdatePokemon: {pokedexID: 0, name: "", type: ""},
+            // AddPokemon: {name: "", type: ""},
+            // DeletePokedexID: {pokedexID: 0},
+            // AddMessage: "",
+            // UpdateMessage: "",
+            // DeleteMessage: "",   
+            // singlePokemon: null,
+            // pokedexIDToGetBy : null,
+        }
+    },
+    
+    methods: {
+        getAllMusicRecords() {
+            this.helperGetAndShow(baseurl)
+        }, 
+
+        async helperGetAndShow(url) {
+            try {
+                const response = await axios.get(url)
+                this.MusicRecords = await response.data
+            } catch (ex) {
+                alert(ex.message) // https://www.w3schools.com/js/js_popup.asp
+            }
+        },
+
+    },
+
+}).mount("#app")
